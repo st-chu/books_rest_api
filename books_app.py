acbaec2323a8920cafff8236347eced1d0ad5a25
Marek Starzyk <stachu.ms9@gmail.com>
2020-12-13 22:09:24 +0100

    Add books_app.py, books.json, models.py

---
+++ books_app.py
@@ -0,0 +1,140 @@
+from flask import Flask, jsonify, request, abort, make_response
+from models import books, is_request_correct, create_new_book
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "Verysecretkey!"
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify({'error': 'Bad Request ', 'status_code': 400}), 400)
+
+
+@app.route("/api/books/", methods=['GET'])
+def books_list_api():
+    return jsonify(books.all()), 200
+
+
+@app.route("/api/books/<int:book_id>", methods=['GET'])
+def get_book(book_id):
+    book = books.get(book_id)
+    if not book:
+        abort(404)
+    return jsonify({"book": book}), 200
+
+
+@app.route("/api/books/", methods=['POST'])
+def create_book():
+    _data = request.json
+    if any([
+        not 'author' in request.json,
+        not 'title' in request.json,
+    ]):
+        abort(400)
+    if is_request_correct(request.json):
+        abort(400)
+    if len(books.all()) == 0:
+        _id = 1
+    else:
+        _id = books.all()[-1]['id'] + 1
+    _book = create_new_book(_id, request.json)
+    books.create(_book)
+    return jsonify({'book': _book}), 201
+
+
+@app.route("/api/books/<int:book_id>", methods=['DELETE'])
+def delete_book(book_id):
+    result = books.delete(book_id)
+    if not result:
+        abort(404)
+    return jsonify({'result': result}), 200
+
+
+@app.route("/api/books/<int:book_id>", methods=['PUT'])
+def update_book(book_id):
+    _book = books.get(book_id)
+    if not _book:
+        abort(404)
+    if not request.json:
+        abort(400)
+    _data = request.json
+    if is_request_correct(request.json):
+        abort(400)
+    _book = {
+        'id': book_id,
+        'author': _data.get('author', _book['author']),
+        'title': _data.get('title', _book['title']),
+        'publisher': _data.get('publisher', _book['publisher']),
+        'description': _data.get('description', _book['description']),
+        'rating': _data.get('rating', _book['rating']),
+        'read': _data.get('read', _book['read']),
+        'genre': _data.get('genre', _book['genre']),
+        'lend': _book['lend']
+    }
+    books.update(book_id, _book)
+    return jsonify({'book': _book}), 200
+
+
+@app.route("/api/books/sort", methods=['POST'])
+def sort_books_list():
+    key_list = ['id', 'author', 'title', 'publisher', 'rating', 'read', 'genre', 'lend']
+    if any([
+        not request.json,
+        not 'key' in request.json,
+        'key' in request.json and not isinstance(request.json['key'], str),
+        'reverse' in request.json and not isinstance(request.json['reverse'], bool),
+        'key' in request.json and not request.json['key'] in key_list
+    ]):
+        abort(400)
+    if 'reverse' not in request.json:
+        reverse = False
+    else:
+        reverse = request.json['reverse']
+    sort_books = books.sort_books(request.json['key'], reverse)
+    return jsonify({f"sorted by key {request.json['key']}": sort_books}), 200
+
+
+@app.route("/api/books/lend/<int:book_id>", methods=['POST'])
+def lend(book_id: int):
+    book = books.get(book_id)
+    if not book:
+        abort(404)
+    if any([
+        'lent' in request.json and not isinstance(request.json['lent'], bool),
+        'who' in request.json and not isinstance(request.json['who'], str),
+    ]):
+        abort(400)
+    if book['lend']['lent'] is True:
+        abort(400)
+    books.lend_book(book, request.json['who'])
+    return jsonify({"lend": books.get(book_id)}), 200
+
+
+@app.route("/api/books/return/<int:book_id>", methods=['POST'])
+def return_book(book_id):
+    book = books.get(book_id)
+    if not book:
+        abort(404)
+    if any({
+        not 'return' in request.json,
+        not isinstance(request.json['return'], bool)
+    }):
+        abort(400)
+    if request.json['return'] is True:
+        if book['lend']['lent'] is True:
+            books.return_book_(book)
+            return jsonify({"return": books.get(book_id)}), 200
+        else:
+            abort(400)
+    else:
+        abort(400)
+
+
+if '__name__' == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,105 @@
+import json
+from typing import Union, Dict, List
+
+
+class Books:
+    def __init__(self):
+        try:
+            with open("books.json", "r") as f:
+                self.books = json.load(f)
+        except FileNotFoundError:
+            self.books = []
+
+    def all(self) -> List[Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]]:
+        return self.books
+
+    def get(self, _id: int) -> Union[List, Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]]:
+        book = [book for book in self.all() if book['id'] == _id]
+        if book:
+            return book[0]
+        return []
+
+    def save_all(self) -> None:
+        with open("books.json", "w") as f:
+            json.dump(self.books, f)
+
+    def create(self, _data: Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]) -> None:
+        self.books.append(_data)
+        self.save_all()
+
+    def update(self, _id: int, _data: Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]) -> bool:
+        book = self.get(_id)
+        if book:
+            index = self.books.index(book)
+            self.books[index] = _data
+            self.save_all()
+            return True
+        return False
+
+    def delete(self, _id: int) -> bool:
+        book = self.get(_id)
+        if book:
+            self.books.remove(book)
+            self.save_all()
+            return True
+        return False
+
+    def sort_books(self, books_to_sort, _key: str, reverse: bool) \
+            -> List[Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]]:
+        sorted_books = books_to_sort.sort(self.all(), key=lambda given_key: given_key[_key], reverse=reverse)
+        return sorted_books
+
+    def lend_book(self, _book: Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]], _who: str) -> None:
+        index = self.books.index(_book)
+        lend = _book['lend']
+        lend['lent'] = True
+        lend['who'] = _who
+        _book.get('lend', lend)
+        self.books[index] = _book
+        self.save_all()
+
+    def return_book_(self, _book: Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]) -> None:
+        index = self.books.index(_book)
+        lend = _book['lend']
+        lend['lent'] = False
+        del lend['who']
+        _book.get('lend', lend)
+        self.books[index] = _book
+        self.save_all()
+
+
+def is_request_correct(_request: Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]) -> bool:
+    try:
+        if any([
+            'author' in _request and not isinstance(_request.get('author'), str),
+            'title' in _request and not isinstance(_request.get('title'), str),
+            'publisher' in _request and not isinstance(_request.get('publisher'), str),
+            'description' in _request and not isinstance(_request.get('description'), str),
+            'rating' in _request and not isinstance(_request.get('rating'), int),
+            'rating' in _request and _request['rating'] not in range(1, 11),
+            'read' in _request and not isinstance(_request.get('read'), bool),
+            'genre' in _request and not isinstance(_request.get('genre'), str),
+            'lend' in _request
+        ]):
+            return True
+    except TypeError:
+        return True
+
+
+def create_new_book(_id: int, _data: Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]])\
+        -> Dict[str, Union[str, int, bool, Dict[str, Union[str, bool]]]]:
+    _book = {
+        'id': _id,
+        'author': _data.get('author'),
+        'title': _data.get('title'),
+        'publisher': _data.get('publisher', 'not entered'),
+        'description': _data.get('description', ''),
+        'rating': _data.get('rating', ''),
+        'read': _data.get('read', False),
+        'genre': _data.get('genre', 'not entered'),
+        'lend': {'lent': False}
+    }
+    return _book
+
+
+books = Books()
